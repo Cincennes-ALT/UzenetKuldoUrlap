803276f1eda5c15da2969ec22516a64ecd1fe17a
Vincent <kemeny.vince@outlook.hu>
2022-08-04 03:10:32 +0200

    form kontroller osztály

---
+++ src/Form/KapcsolatControllerFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Kapcsolat;
+// hiba? use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class KapcsolatControllerFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nev', TextType::class)
+            ->add('Email', EmailType::class)
+            ->add('Uzenet', TextareaType::class)
+            ->add('Bekuldes', SubmitType::class);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+            'data_class' => Kapcsolat::class,
+        ]);
+    }
+}
